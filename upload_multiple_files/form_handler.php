0ddee1bec97adaca12c47ee9e2dc06be5f842fb6
Daria <dashulya-pustohailova@mail.ru>
2020-06-30 04:59:07 +0300

    upload multiple files

--- upload_multiple_files/form_handler.php
+++ upload_multiple_files/form_handler.php
@@ -2,48 +2,64 @@
 
 function start()
 {
+    $addGood = 0;
+    $addBad = 0;
     $files = $_FILES['picture'];
-    var_dump($files);
+   var_dump($files);
     $my_count = count($files['name']); // количество файлов, которое хотим загрузить
-    echo"Количество загружаемых файлов = $my_count";
+    echo"Количество загружаемых файлов = $my_count<br>";
 
     for ($i = 0; $i < $my_count; $i++) {
+        $name = $files['name'][$i];
 
 
         //определяем расширение очередного файла
         $eth = pathinfo($files['name'][$i], PATHINFO_EXTENSION);
-        var_dump($eth);
+//        var_dump($eth);
 
         //Если произошла ошибка(не тот тип или размер - выходим)
-        if (chkType() || chkSize()){
-
+        if ($err = chkType($eth)){
+            printErrors($name, $err);
+            $addBad++;
+            continue;
+        }
+        else if($err = chkSize(3000,$files['size'][$i])){
+            printErrors($name, $err);
+            $addBad++;
+            continue;
         }
 
 
         //хэшируем строки и приклеиваем расширение
-        $name = $files['name'][$i];
         $new_name = md5($files['name'][$i].microtime().rand(0,99)) . ".$eth";
-        var_dump($new_name);
+//        var_dump($new_name);
 
         //перемещаем из временной папки
         if(move_uploaded_file($files['tmp_name'][$i], "img/$new_name")){
-           echo "Файл  $name успешно загружен";
+           echo "Файл  $name успешно загружен<br>";
+           $addGood++;
         }
         else{
-            echo "Файл  $name загрузить не удалось(отстутвует папка)";
+            echo "Файл  $name загрузить не удалось(отстутвует папка)<br>";
         }
     }
+
+    echo "Успешно загружено $addGood из $my_count<br>";
+    echo "Ошибка при загрузке в $addBad из $my_count";
 }
 
-function chkType(){
-   // $err_type = 0;
-    //return $err_type; // вернет код ошибки и 0 - если все ок
+function chkType($eth){
+    $goodType = ['png', 'jpg'];
+    if(!in_array($eth, $goodType)){
+        return 9;
+    }
     return 0;
 }
 
-function chkSize(){
-    //$err_type = 0;
-   // return $err_type; // вернет код ошибки и 0 - если все ок
+function chkSize($sizeGood, $size){
+    if($size > $sizeGood){
+        return 2;
+    }
     return 0;
 }
 
@@ -51,18 +67,22 @@ function printErrors($nameFile, $err) {
     switch ($err){
         case 1:
         case 2:
-            echo "Недопустимый размер файла $nameFile";
+            echo "Недопустимый размер файла $nameFile<br>";
             break;
         case 3:
         case 4:
         case 8:
-            echo "Файл $nameFile не был загружен" ;
+            echo "Файл $nameFile не был загружен<br>" ;
             break;
         case 6:
-            echo "Файл $nameFile не был загружен, т.к. отсутсвует временная папка";
+            echo "Файл $nameFile не был загружен, т.к. отсутсвует временная папка<br>";
             break;
         case 7:
-            echo "Не удалось записать файл $nameFile на диск";
+            echo "Не удалось записать файл $nameFile на диск<br>";
+            break;
+        //не нашла код ошибки, который отвечает за тип
+        case 9:
+            echo "Недопустимый формат $nameFile<br>";
             break;
     }
 }
